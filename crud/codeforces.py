a557de68c6d3dac45ff888a2d1d00835ec645b58
ColtenOuO <jun930436@gmail.com>
2024-03-22 17:12:31 +0800

    fixed bugs

--- crud/codeforces.py
+++ crud/codeforces.py
@@ -1,7 +1,7 @@
 import requests
 import json
 class CodeforcesData:
-    def get_rating_by_handle(handle: str) -> int:
+    def get_rating_by_handle(self,handle: str) -> int:
         url = "https://codeforces.com/api/user.info"
         data = { "handles": handle }
         access_token = requests.post(url, data = data)
